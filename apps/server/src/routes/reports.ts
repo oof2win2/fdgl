f98800c48ed2d82966a62d146f04e6a93ad53524
oof2win2 <honza.koco44@gmail.com>
2024-02-17 10:52:34 +0000

    start implementing report creation

--- apps/server/src/routes/reports.ts
+++ apps/server/src/routes/reports.ts
@@ -1,9 +1,23 @@
-import { Router } from "itty-router";
+import { Router, error } from "itty-router";
 import type { CF, RequestType } from "../types";
 import type { Expression } from "kysely";
 import type { DB } from "../db-types";
 import type { SqlBool } from "kysely";
 import type { StringReference } from "kysely";
+import {
+	array,
+	integer,
+	maxValue,
+	minLength,
+	minValue,
+	number,
+	object,
+	string,
+} from "valibot";
+import { getJSONBody, type JSONParsedBody } from "../utils/json-body";
+import { communityAuthorize, type AuthorizedRequest } from "../utils/auth";
+import { nanoid } from "nanoid";
+import { datePlus } from "itty-time";
 
 const ReportsRouter = Router({ base: "/reports" });
 
@@ -69,5 +83,72 @@ ReportsRouter.get<RequestType, CF>("/", async (req, env) => {
 	const results = reports.execute();
 	return results;
 });
+
+// POST /
+// create a report
+const createReportSchema = object({
+	playername: string(),
+	description: string(),
+	createdBy: string(),
+	categoryIds: array(string(), [minLength(1)]),
+	proofCount: number([integer(), minValue(0), maxValue(25)]),
+});
+ReportsRouter.put<
+	AuthorizedRequest<JSONParsedBody<typeof createReportSchema>>,
+	CF
+>(
+	"/",
+	communityAuthorize,
+	getJSONBody(createReportSchema),
+	async (req, env) => {
+		const reportId = nanoid();
+		const body = req.jsonParsedBody;
+		try {
+			await env.kysely
+				.insertInto("Report")
+				.values({
+					id: reportId,
+					playername: body.playername,
+					communityId: req.communityId,
+					createdAt: new Date().toISOString(),
+					expiresAt: datePlus("6 months").toISOString(),
+					description: body.description,
+					createdBy: body.createdBy,
+				})
+				.execute();
+			await env.kysely
+				.insertInto("ReportCategory")
+				.values(
+					body.categoryIds.map((categoryId) => ({
+						reportId: reportId,
+						categoryId,
+					}))
+				)
+				.execute();
+			// TODO: handle uploading proof
+			// const generatedReportProof
+			// await env.kysely.insertInto("ReportProof").values
+			return {
+				id: reportId,
+				proofURLs: [],
+			};
+		} catch {
+			// delete the report data if it happened to get inserted
+			await env.kysely
+				.deleteFrom("Report")
+				.where("id", "=", reportId)
+				.execute();
+			await env.kysely
+				.deleteFrom("ReportCategory")
+				.where("reportId", "=", reportId)
+				.execute();
+			await env.kysely
+				.deleteFrom("ReportProof")
+				.where("reportId", "=", reportId)
+				.execute();
+			return error(500, { message: "Creating report failed" });
+		}
+	}
+);
 
 export default ReportsRouter;
