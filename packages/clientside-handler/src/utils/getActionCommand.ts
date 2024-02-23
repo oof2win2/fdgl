9afb63d4f70865e65ee6ec0109dd4e35df61d491
oof2win2 <honza.koco44@gmail.com>
2024-02-23 09:18:07 +0100

    add all available fields into command execution

--- packages/clientside-handler/src/utils/getActionCommand.ts
+++ packages/clientside-handler/src/utils/getActionCommand.ts
@@ -3,15 +3,30 @@ import type { Action } from "../types";
 
 export function getExecuteCommand(action: Action, report: Report) {
 	return action.runCommand
-		.replaceAll("{PLAYERNAME}", report.playername)
 		.replaceAll("{REPORTID}", report.id)
+		.replaceAll("{PLAYERNAME}", report.playername)
+		.replaceAll(
+			"{DESCRIPTION}",
+			report.description ?? "No description provided"
+		)
+		.replaceAll("{CREATEDBY}", report.createdBy)
 		.replaceAll("{COMMUNITYID}", report.communityId)
+		.replaceAll("{CATEGORYIDS}", report.categoryIds.join(","))
+		.replaceAll("{PROOFLINKS}", report.proofLinks.join(","))
 		.replaceAll("{CREATEDAT}", report.createdAt);
 }
 export function getUndoCommand(action: Action, revocation: Revocation) {
 	return action.undoCommand
-		.replaceAll("{PLAYERNAME}", revocation.playername)
 		.replaceAll("{REPORTID}", revocation.id)
+		.replaceAll("{PLAYERNAME}", revocation.playername)
+		.replaceAll(
+			"{DESCRIPTION}",
+			revocation.description ?? "No description provided"
+		)
+		.replaceAll("{CREATEDBY}", revocation.createdBy)
 		.replaceAll("{COMMUNITYID}", revocation.communityId)
-		.replaceAll("{CREATEDAT}", revocation.createdAt);
+		.replaceAll("{CATEGORYIDS}", revocation.categoryIds.join(","))
+		.replaceAll("{PROOFLINKS}", revocation.proofLinks.join(","))
+		.replaceAll("{CREATEDAT}", revocation.createdAt)
+		.replaceAll("{REVOKEDAT}", revocation.revokedAt);
 }
