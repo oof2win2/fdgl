31c04ebecb1bc87f5cb1386a2474f65ece803d88
oof2win2 <honza.koco44@gmail.com>
2024-04-01 15:15:53 +0200

    remove prisma and kysely remains

--- apps/server/src/router.ts
+++ apps/server/src/router.ts
@@ -1,28 +1,28 @@
 import { Router, error } from "itty-router";
 import { type CF, type RequestType } from "./types";
-// import categoriesRouter from "./routes/categories";
-// import masterCategoriesRouter from "./routes/categories-master";
-// import masterCommunitiesRouter from "./routes/communities-master";
-// import ReportsRouter from "./routes/reports";
-// import { MasterAuthenticate } from "./utils/auth";
-// import communitiesRouter from "./routes/communities";
+import categoriesRouter from "./routes/categories";
+import masterCategoriesRouter from "./routes/categories-master";
+import masterCommunitiesRouter from "./routes/communities-master";
+import ReportsRouter from "./routes/reports";
+import { MasterAuthenticate } from "./utils/auth";
+import communitiesRouter from "./routes/communities";
 
 const router = Router<RequestType, CF>();
 
 router
-	// .all("/categories/*", categoriesRouter.handle)
-	// .all("/communities/*", communitiesRouter.handle)
-	// .all("/reports/*", ReportsRouter.handle)
-	// .all(
-	// 	"/master/categories/*",
-	// 	MasterAuthenticate,
-	// 	masterCategoriesRouter.handle,
-	// )
-	// .all(
-	// 	"/master/communities/*",
-	// 	MasterAuthenticate,
-	// 	masterCommunitiesRouter.handle,
-	// )
+	.all("/categories/*", categoriesRouter.handle)
+	.all("/communities/*", communitiesRouter.handle)
+	.all("/reports/*", ReportsRouter.handle)
+	.all(
+		"/master/categories/*",
+		MasterAuthenticate,
+		masterCategoriesRouter.handle,
+	)
+	.all(
+		"/master/communities/*",
+		MasterAuthenticate,
+		masterCommunitiesRouter.handle,
+	)
 	.all("*", () => error(404));
 
 export default router;
