4751bf5b89b4e672ce7e09dc83063a8c320f5409
oof2win2 <honza.koco44@gmail.com>
2024-05-08 09:10:16 +0200

    types: add system update notifications

--- packages/types/src/baseTypes.ts
+++ packages/types/src/baseTypes.ts
@@ -5,12 +5,13 @@ import {
 	merge,
 	object,
 	string,
+	union,
 } from "valibot";
 
 export const Report = object({
 	id: string(),
 	playername: string(),
-	description: nullable(string()),
+	description: string(),
 	createdBy: string(),
 	communityId: string(),
 
@@ -21,6 +22,16 @@ export const Report = object({
 	isRevoked: literal(false),
 });
 export type Report = Output<typeof Report>;
+
+export const CreateReport = object({
+	playername: string(),
+	description: string(),
+	createdBy: string(),
+
+	categoryIds: array(string()),
+	proofLinks: array(string()),
+});
+export type CreateReport = Output<typeof CreateReport>;
 
 export const Revocation = merge([
 	Report,
@@ -50,3 +61,45 @@ export const FilterObject = object({
 	communityFilters: array(string()),
 });
 export type FilterObject = Output<typeof FilterObject>;
+
+export const CommunityCreationNotification = object({
+	type: literal("communityCreation"),
+	communityId: string(),
+});
+export type CommunityCreationNotification = Output<
+	typeof CommunityCreationNotification
+>;
+
+export const CommunityMergeNotification = object({
+	type: literal("communityMerge"),
+	sourceId: string(),
+	targetId: string(),
+});
+export type CommunityMergeNotification = Output<
+	typeof CommunityMergeNotification
+>;
+
+export const CategoryCreationNotification = object({
+	type: literal("categoryCreated"),
+	categoryId: string(),
+});
+export type CategoryCreationNotification = Output<
+	typeof CategoryCreationNotification
+>;
+
+export const CategoryMergedNotification = object({
+	type: literal("categoryMerged"),
+	sourceId: string(),
+	targetId: string(),
+});
+export type CategoryMergedNotification = Output<
+	typeof CategoryMergedNotification
+>;
+
+export const SystemUpdateNotification = union([
+	CommunityCreationNotification,
+	CommunityMergeNotification,
+	CategoryCreationNotification,
+	CategoryMergedNotification,
+]);
+export type SystemUpdateNotification = Output<typeof SystemUpdateNotification>;
