a88823295506b371bef147b79e62480fb6161d9e
oof2win2 <honza.koco44@gmail.com>
2024-05-18 10:52:20 +0200

    remove s3 from backend env

--- apps/backend/src/utils/env.ts
+++ apps/backend/src/utils/env.ts
@@ -3,9 +3,20 @@ import * as z from "zod";
 const schema = z.object({
 	MASTER_API_KEY: z.string({ description: "Master API key for the API" }),
 	DATABASE_URL: z.string({ description: "URL of the SQLite database" }),
-	S3_BUCKET_URL: z.string().url(),
-	S3_ACCESS_KEY_ID: z.string(),
-	S3_SECRET_ACCESS_KEY: z.string(),
+	// S3_BUCKET_URL: z.string().url(),
+	// S3_ACCESS_KEY_ID: z.string(),
+	// S3_SECRET_ACCESS_KEY: z.string(),
 });
 
-export const ENV = schema.parse(Bun.env);
+let env: z.infer<typeof schema>;
+
+const parsed = schema.safeParse(Bun.env);
+
+if (parsed.success) env = parsed.data;
+else {
+	const err = parsed.error;
+	console.log("❌ Invalid environment variables:", err.flatten().fieldErrors);
+	throw new Error("Invalid environment variables");
+}
+
+export const ENV = env;
--- apps/backend/src/utils/parseZodIssues.ts
+++ apps/backend/src/utils/parseZodIssues.ts
@@ -9,10 +9,8 @@ type ErrorResponse = {
 };
 export function parseZodIssues(error: ZodError): ErrorResponse {
 	const issues = error.issues;
-	console.log(issues);
 	const errors = issues.map((issue) => {
 		const fullPath = issue.path.length ? issue.path.join("/") : "/";
-		console.log(issue);
 		return {
 			path: fullPath,
 			message: issue.message,
